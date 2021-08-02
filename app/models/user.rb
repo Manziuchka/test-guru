0976a6c3ae7bf4c54a74d1fab97dee614b9762f1
Manziuchka <Manziuk.olga@gmail.com>
2021-08-03 00:55:13 +0300

    add list_of_tests and tests_by_category

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  def list_of_tests(level)
+    Test.joins("JOIN user_tests ON user_tests.test_id = tests.id")
+    .where("tests.level = ?", level)
+    .pluck("tests.title")
+  end
 end
