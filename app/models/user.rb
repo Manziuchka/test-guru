48e846b26390b8d16041e4f28770853f5122512f
Manziuchka <Manziuk.olga@gmail.com>
2021-08-03 18:48:06 +0300

    fix list_of_tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   def list_of_tests(level)
     Test.joins("JOIN user_tests ON user_tests.test_id = tests.id")
-    .where("tests.level = ?", level)
-    .pluck("tests.title")
+    .where(level: level, user_tests: {user_id: self.id})
   end
 end
